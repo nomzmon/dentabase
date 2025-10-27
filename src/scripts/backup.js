daf7522b2c3c4892d101f3184267b8f7dedcc54c
expmine <austinvgan@gmail.com>
2025-10-28 04:28:23 +0800

    backup change bug fixed + additional unit test case

--- src/scripts/backup.js
+++ src/scripts/backup.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
 const mongoose = require('mongoose');
+const { ObjectId } = require('mongodb');
 
 function getTimestamp() {
   const now = new Date();
@@ -61,32 +62,42 @@ async function loadBackup(baseDir) {
     const newIds = newData.map(doc => doc._id?.toString()).filter(Boolean);
     const existingIds = existingData.map(doc => doc._id?.toString()).filter(Boolean);
 
-    // Detect documents to insert
+    // Detect inserts
     const toInsert = newData.filter(doc => !existingIds.includes(doc._id?.toString()));
 
-    // Detect documents to delete
+    // Detect deletes
     const toDelete = existingData.filter(doc => !newIds.includes(doc._id?.toString()));
 
-    if (toInsert.length === 0 && toDelete.length === 0) {
+    // Detect updates
+    const toUpdate = newData.filter(newDoc => {
+      const match = existingData.find(e => e._id?.toString() === newDoc._id?.toString());
+      return match && JSON.stringify(match) !== JSON.stringify(newDoc);
+    });
+
+    if (toInsert.length === 0 && toDelete.length === 0 && toUpdate.length === 0) {
       console.log(`Collection '${collectionName}' is already up-to-date`);
       continue;
     }
 
-    // Perform insertions
-    if (toInsert.length > 0) {
-      await collection.insertMany(toInsert);
-    }
-
-    // Perform deletions
+    if (toInsert.length > 0) await collection.insertMany(toInsert);
+    
     if (toDelete.length > 0) {
       const deleteIds = toDelete.map(doc => doc._id);
       await collection.deleteMany({ _id: { $in: deleteIds } });
     }
 
+    for (const doc of toUpdate) {
+      const { _id, ...fields } = doc;
+      const queryId = ObjectId.isValid(_id) ? new ObjectId(_id) : _id;
+      await collection.updateOne({ _id: queryId }, { $set: fields });
+    }
+
+
     console.log(
-      `Updated collection '${collectionName}': inserted ${toInsert.length}, deleted ${toDelete.length}`
+      `Updated '${collectionName}': inserted ${toInsert.length}, updated ${toUpdate.length}, deleted ${toDelete.length}`
     );
   }
 }
+
 
 module.exports = { saveBackup, loadBackup };
